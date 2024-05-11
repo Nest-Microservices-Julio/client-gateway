fd839e66ec72ce484ca6787f2f0a3c1730969419
Julio Donado <jalfredodonado@itsa.edu.co>
2024-05-11 14:09:37 -0500

    Github submodules iniciar

--- src/common/exceptions/rpc-custom-exepctions.filter.ts
+++ src/common/exceptions/rpc-custom-exepctions.filter.ts
@@ -9,6 +9,15 @@ export class RpcCustomExceptionFilter implements ExceptionFilter<RpcException> {
 
     const rpcError = exception.getError();
 
+    console.log(rpcError.toString());
+    
+    if (rpcError.toString().toLowerCase().includes("Empty Response".toLowerCase())) {
+      return response.status(500).json({
+        status: 500,
+        error: rpcError.toString().substring(0, rpcError.toString().indexOf("(") - 1)
+      });
+    }
+
     if (typeof rpcError === "object" && 'status' in rpcError && 'message' in rpcError) {
       // @ts-ignore
       const status = isNaN(rpcError.status) ? 400 : +rpcError.status;
