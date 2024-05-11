f8c5cf2d45a5212fda8832885ce17a351e3599a0
Julio Donado <jalfredodonado@itsa.edu.co>
2024-05-11 14:33:33 -0500

    chore: Update console log statement in main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log("Hola mundo, primer cambio")
+  console.log("Hola mundo, segundo cambio")
 
   logger.log(`Server is running on http://localhost:${envs.port}`);
 }
